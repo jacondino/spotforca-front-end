542c5a8ca5bdf63370573e76f99fffc50fa2df12
Anderson Fernando Lopes Jacondino <anderson.jacondino@cwi.com.br>
2019-09-24 14:28:53 -0300

    validações e css

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -8,7 +8,7 @@
     <h1 v-if="error>6">perna direita</h1>
     <div class="words">
       <div v-for="(item, i) in words.split('').length" :key="i">
-        <span>{{ i }}</span>
+        <span v-if="wordsSuccess.filter((it) => it ===  words[i]).length > 0">{{ words[i] }}</span>
       </div>
     </div>
     <input v-model="wordsClient" maxlength="1" />
@@ -25,11 +25,29 @@ export default {
     wordsError: [],
     wordsSuccess: [],
     wordsClient: "",
-    err: false,
+    err: false
   }),
   methods: {
     onCheckLetters() {
       const p = this.words.split("");
+
+      if (!this.wordsClient) return;
+
+      const checkSuccess = this.wordsSuccess.filter(
+        it => it.toUpperCase() == this.wordsClient.toUpperCase()
+      );
+
+      const checkError = this.wordsError.filter(
+        it => it.toUpperCase() == this.wordsClient.toUpperCase()
+      );
+
+      if (checkSuccess.length > 0 || checkError.length > 0) {
+        this.wordsClient = "";
+        this.err = false;
+
+        return;
+      }
+
       p.map(it => {
         if (it.toUpperCase() === this.wordsClient.toUpperCase()) {
           this.wordsSuccess.push(it);
@@ -50,20 +68,22 @@ export default {
     }
   }
 };
-// .palavras div {
-//     margin: 1em;
-//     border-bottom: 5px solid;
-//     width: 32px;
-//     padding-bottom: 6px;
-// }
-// .palavra {
-//   display: flex;
-//     flex-wrap: wrap;
-//     justify-content: center;
-// }
-// uma palavra
-// usuario digita letra
-// se estiver errado error recebe +1 e
-// LetrasErradas
-// se estiver certo LetrasCorretas
 </script>
+
+<style scoped>
+.words {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.words div {
+  margin: 1em;
+  border-bottom: 5px solid;
+  width: 32px;
+  padding-bottom: 6px;
+}
+.words div span {
+  font-size: 2em;
+  font-weight: 400;
+}
+</style>
